0311ccc4146728c96297d79d24cf1afc49db49c2
Yao-TAT <805787173@qq.com>
2020-05-09 20:59:34 +0800

    four Myjava

--- src/iderars/Helloword.java
+++ src/iderars/Helloword.java
@@ -2,11 +2,9 @@ package iderars;
 
 public class Helloword {
     public static void main(String [] args){
+        int number = 2;
         System.out.println("HelloWorld!");
         System.out.println("JAVA");
-        System.out.println("JAVA");System.out.println("JAVA");
-        System.out.println("JAVA");
-        System.out.println("JAVA");
-
+        System.out.println(number);
     }
 }
