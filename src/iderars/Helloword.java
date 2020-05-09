39e50f4d34a84d60aa1da1e4aab0b763d83e92a1
Yao-TAT <805787173@qq.com>
2020-05-09 20:51:46 +0800

    three GitJava

--- src/iderars/Helloword.java
+++ src/iderars/Helloword.java
@@ -4,5 +4,9 @@ public class Helloword {
     public static void main(String [] args){
         System.out.println("HelloWorld!");
         System.out.println("JAVA");
+        System.out.println("JAVA");System.out.println("JAVA");
+        System.out.println("JAVA");
+        System.out.println("JAVA");
+
     }
 }
